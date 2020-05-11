4f59564b63d42bf487008f2bba1646186c12d2ae
Kireet Gannavarapu <thekireet@gmail.com>
2020-05-11 23:34:12 +0530

    Converted Home into a ejs template and connected compose and homepage

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,6 +18,7 @@ const articleSchema = {
   author: String,
   content: String,
   imageUrl: String,
+  genre: String,
   month: Number,
   year: Number
 };
@@ -24,11 +26,45 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 app.get("/", function(req, res){
-  res.render("home");
+  const date = new Date();
+  const currentMonth = date.getMonth() + 1;
+  const currentYear = date.getFullYear();
+  Article.find({month: currentMonth, year: currentYear}, function(err, results){
+    if(err){
+      console.log(err);
+    }else{
+      if(results){
+        res.render("home", {
+          pageTitle: "New Articles",
+          articles: results
+        });
+      }
+    }
+  });
 });
 
 app.get("/compose", function(req, res){
   res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const articleDetails = new Article({
+    title: req.body.title,
+    author: req.body.authorName,
+    content: req.body.content,
+    imageUrl: req.body.image,
+    genre: req.body.genre,
+    month: req.body.month,
+    year: req.body.year
+  });
+
+  articleDetails.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }else{
+      console.log(err);
+    }
+  });
 });
 
 
