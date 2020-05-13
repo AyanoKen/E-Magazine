bf0b38d363f50c15b09d370f3068575ddef94bfb
Kireet Gannavarapu <thekireet@gmail.com>
2020-05-13 16:36:04 +0530

    Added procfile and updated ports

--- app.js
+++ app.js
@@ -227,6 +227,11 @@ app.get("/logout", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server is up and running");
-})
+});
